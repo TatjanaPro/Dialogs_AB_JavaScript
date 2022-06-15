4e415c2fc7448742852d181a7839dd94912be061
TatjanaPro <tatjaana.pronina@gmail.com>
2022-06-15 21:05:07 +0300

    Arrays

--- Class_09_2022_06_13/script.js
+++ Class_09_2022_06_13/script.js
@@ -64,3 +64,18 @@ document.getElementById("bin_peec_algoritma").innerHTML = bit_sequence;
 
 document.getElementById("bin_peec_standarta_funkcijas").innerHTML =
   Number(x).toString(2);
+
+var vards_uzvards = "Jānis Bērziņš,";
+var nauda = 15000;
+var peļņa_1 = (nauda * 0, 01) * 1;
+var peļņa_2 = (nauda * 0, 02) * 2;
+var peļņa_3 = (nauda * 0, 03) * 3;
+var peļņa_4 = (nauda * 0, 04) * 4;
+var peļņa_5 = (nauda * 0, 05) * 5;
+document.getElementById("client").innerHTML = vards_uzvards;
+document.getElementById("money").innerHTML = nauda;
+document.getElementById("procenti_1").innerHTML = peļņa_1;
+document.getElementById("procenti_2").innerHTML = peļņa_2;
+document.getElementById("procenti_3").innerHTML = peļņa_3;
+document.getElementById("procenti_4").innerHTML = peļņa_4;
+document.getElementById("procenti_5").innerHTML = peļņa_5;
---
+++ Class_09_2022_06_15/script.js
@@ -0,0 +1,16 @@
+// papildu uzdevums - attelot summas ar divam zimem aiz komata
+//ja % izmaksa notiek 1 reizi termina beigas
+var name = "Jānis Bērziņš,";
+var money = 15000;
+var profit_1 = money * 0.0001 * 1;
+var profit_2 = money * 0.0002 * 2;
+var profit_3 = money * 0.0003 * 3;
+var pprofit_4 = money * 0.0004 * 4;
+var pprofit_5 = money * 0.0005 * 5;
+document.getElementById("client").innerHTML = name;
+document.getElementById("money").innerHTML = money;
+document.getElementById("procenti_1").innerHTML = profit_1;
+document.getElementById("procenti_2").innerHTML = profit_2;
+document.getElementById("procenti_3").innerHTML = profit_3;
+document.getElementById("procenti_4").innerHTML = profit_4;
+document.getElementById("procenti_5").innerHTML = profit_5;

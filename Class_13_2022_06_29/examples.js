f51c64f8cc90f76027128337ef8e57def8f35a21
TatjanaPro <tatjaana.pronina@gmail.com>
2022-07-03 21:58:54 +0300

    Loops theory and examples

---
+++ Class_13_2022_06_29/examples.js
@@ -0,0 +1,34 @@
+//You can initiate many values in statement 1 (separated by comma):
+id = "demo";
+
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+let i, len, text;
+for (i = 0, len = cars.length, text = ""; i < len; i++) {
+  text += cars[i] + "<br>";
+}
+document.getElementById("demo").innerHTML = text;
+
+//using var, the variable declared in the loop redeclares the variable outside the loop. Answer i = 10
+
+var a = 5;
+for (var a = 0; a < 10; a++) {
+  // some statements
+}
+document.getElementById("demo").innerHTML = a;
+
+//using let, the variable declared in the loop does not redeclare the variable outside the loop. Answer i = 5
+
+let b = 5;
+for (let b = 0; b < 10; b++) {}
+document.getElementById("demo").innerHTML = b;
+
+//The for in statement loops through the properties of an object:John Doe 25
+
+const person = { fname: "John", lname: "Doe", age: 25 };
+
+let txt = "";
+for (let x in person) {
+  txt += person[x] + " ";
+}
+
+document.getElementById("demo").innerHTML = txt;
--- Class_13_2022_06_29/script.js
+++ Class_13_2022_06_29/script.js
@@ -1,14 +1,45 @@
 console.log("Skripta izpildes sakums:\n" + new Date());
 document.getElementById("manual_count").innerHTML = "1<br>2<br>3<br>4<br>";
 
-let text = "";
+let text_ = "";
 for (let i = 1; i < 11; i++) {
   if (i % 2 == 0) {
-    text = text + "para skaitlis " + i + "<br>";
+    text = text_ + "para skaitlis " + i + "<br>";
   } else {
-    text = text + "nepara skaitlis" + i + "<br>";
+    text = text_ + "nepara skaitlis" + i + "<br>";
   }
 }
-document.getElementById("automatic_count").innerHTML = text;
+document.getElementById("automatic_count").innerHTML = text_;
 
 //  text += cars[0] + "<br>"; --> text = text + cars[0] + "<br>";
+
+text = "",
+var dalit = [2, 3, 4, 6, 8];
+
+for (let dalam = 1; dalam < 25; dalam++) {
+  let flag=1;
+  text = text + dalam;
+  for (let k = 0; k < dalit.length; k++) {
+    if (dalam % dalit[k] == 0) {
+      if (flag ==1)
+        {
+          text = text + " dalās ar skaitli ";
+          flag = 0 ; 
+        }
+      text = text + dalit[k] + ",";
+    }
+  }
+  if (flag==0)
+  {
+    text=text.slice(0,text.length-1);
+  }
+
+
+  if (dalam % 2 == 0) {
+    text = text + " pāra skaitlis ";
+  } else {
+    text = text + " nepāra skaitlis ";
+  }
+  text = text + "<BR>";
+}
+document.getElementById("automatic_count").innerHTML = text;
